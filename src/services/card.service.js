2f064a16699151cffb268803ed99b050b6a971b9
ak8431 <amitkumarmaurya8431@gmail.com>
2023-12-26 13:02:28 +0530

    added pull request log API

--- src/services/card.service.js
+++ src/services/card.service.js
@@ -1,18 +1,18 @@
 import axios from "axios";
-// import authHeader from "./auth-header";
+import authHeader from "./auth-header";
 
 const CARD_URL = `${process.env.REACT_APP_URL}/api/cardtrack/`;
 
 const getCardTrackingList = () => {
-  return axios.get(CARD_URL + "all");
+  return axios.get(CARD_URL + "all", { headers: authHeader() });
 };
 
 const updateCardTrackingList = (id, data) => {
-  return axios.put(CARD_URL + id, data);
+  return axios.put(CARD_URL + id, data, { headers: authHeader() });
 };
 
 const getAllCardsWithFileDeatils = () => {
-  return axios.get(CARD_URL + 'all?fileList=true');
+  return axios.get(CARD_URL + 'all?fileList=true', { headers: authHeader() });
 };
 
 const CardTrackingService = {
--- src/services/pull-request.service.js
+++ src/services/pull-request.service.js
@@ -1,17 +1,18 @@
 import axios from "axios";
+import authHeader from './auth-header';
 
 const PULL_REQUEST_URL = `${process.env.REACT_APP_URL}/api/pull-request/`;
 
 const getPullRequest = () => {
-  return axios.get(PULL_REQUEST_URL + "all");
+  return axios.get(PULL_REQUEST_URL + "all", { headers: authHeader() });
 };
 
 const getPullRequestById = (pullReqId) => {
-    return axios.get(PULL_REQUEST_URL + pullReqId);
+    return axios.get(PULL_REQUEST_URL + pullReqId, { headers: authHeader() });
 };
 
 const createPullRequest = (data) => {
-    return axios.post(PULL_REQUEST_URL, data);
+    return axios.post(PULL_REQUEST_URL, data, { headers: authHeader() });
 };
 
 const FlieMasterListService = {
